cefa29f4f6b67274bc1ecd3999b22a5a775cbaf1
Pham Thi Van <phamthivan@Vans-Mac.local>
2019-02-28 13:56:36 +0700

    Updates

---
+++ precache-manifest.642fa4e61d3d6596fcb5a82f232fada2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.fd78f13f.js"
+  },
+  {
+    "revision": "582e157db4aff051e3af",
+    "url": "/resume/static/js/main.582e157d.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/resume/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b839ac1a19ce06bec80ba59daaa604c3",
+    "url": "/resume/index.html"
+  }
+];
